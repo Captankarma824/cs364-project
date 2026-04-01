263394275186838f4a64853e346c725dcb1547b8
Kyle Matthai <matthai9755@uwlax.edu>
2026-04-01 16:34:03 -0500

    made backend and connected to sql server

--- app.js
+++ app.js
@@ -20,23 +20,17 @@ connection.connect((err) => {
   console.log('Connected to the SQL database!');
 });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+//middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//routes
+app.use('/',sqlRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
